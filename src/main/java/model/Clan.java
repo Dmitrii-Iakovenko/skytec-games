82ec2b93b09f2848cf7226894543d61f8ca39141
Dmitrii Iakovenko <dmitrii.iakovenko84@gmail.com>
2024-01-16 16:44:43 +0600

    Add terms of reference

---
+++ src/main/java/model/Clan.java
@@ -0,0 +1,8 @@
+package model;
+
+// Упрощенный объект клана
+public class Clan {
+    private long id;     // id клана
+    private String name; // имя клана
+    private int gold;    // текущее количество золота в казне клана
+}
---
+++ src/main/java/service/ClanService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Clan;
+
+// Есть сервис, посвященный кланам.
+// Да это выглядит как 'репозиторий'.
+// Но это сервис, просто все остальные методы не нужны для примера
+public interface ClanService {
+    Clan get(long clanId);
+}
